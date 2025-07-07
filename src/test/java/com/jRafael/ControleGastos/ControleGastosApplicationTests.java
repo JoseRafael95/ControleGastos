6b21cc58a576c39bd64ec8c50507fe0fe35fc6d6
Jose Rafael <joserafael.x95@gmail.com>
2025-07-07 12:18:28 -0300

    Firt commit: inicializa estrutura do projeto ControleGastos - Configuração básica gerada pelo Spring Initializr.

---
+++ src/test/java/com/jRafael/ControleGastos/ControleGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jRafael.ControleGastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
